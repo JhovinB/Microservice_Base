6cefc490d581ff87924e57ea95367266ed97f8db
JhovinB <jbac.tec@gmail.com>
2021-08-17 12:48:17 -0500

    Config timeout Hystrix and Riboon

--- microservice_items/src/main/java/com/jbac/app/items/controller/ItemRestController.java
+++ microservice_items/src/main/java/com/jbac/app/items/controller/ItemRestController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jbac.app.items.models.Item;
+import com.jbac.app.items.models.Product;
 import com.jbac.app.items.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/api/v1/items")
@@ -25,8 +27,21 @@ public class ItemRestController {
 		return itemService.findAll();
 	}
 	
+	@HystrixCommand(fallbackMethod = "alternativeMethod")
 	@GetMapping("/{id}/quantity/{quantity}")
 	public Item getItem(@PathVariable("id") Long id, @PathVariable("quantity") Integer quantity) {
 		return itemService.findById(id, quantity);
+	}
+	//Simulando un fallo
+	public Item alternativeMethod(Long id,Integer quantity) {
+		
+		Item item = new Item();
+		Product product = new Product();
+		item.setQuantity(quantity);	
+		product.setName("Iphone 11");
+		product.setPrice(1200.00);
+		item.setProduct(product);
+		return item;
+
 	}
 }

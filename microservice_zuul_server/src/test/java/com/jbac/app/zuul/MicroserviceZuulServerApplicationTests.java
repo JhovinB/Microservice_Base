0ee48f67ff887ae178ce195738992d4c8df27fc0
JhovinB <jbac.tec@gmail.com>
2021-08-17 22:18:21 -0500

    Config Zuul

---
+++ microservice_zuul_server/src/test/java/com/jbac/app/zuul/MicroserviceZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbac.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
